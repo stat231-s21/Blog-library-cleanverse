1a3dde78d9a76d5c3cc8db2bf49857f95387188e
kverma14 <68126242+kverma14@users.noreply.github.com>
2021-05-17 22:23:17 -0400

    kv final shiny

--- kriti/app.R
+++ kriti/app.R
@@ -7,7 +7,7 @@ library(ggrepel)
 ###############
 # import data #
 ###############
-kv_data <- read_csv("us-daily-covid-vaccine-doses-administered_final2.csv") 
+kv_data <- read_csv("/Users/kritiverma/Documents/Amherst Junior Year/Spring 2021/KV Data Science/git/Blog-library-cleanverse/kriti/us-daily-covid-vaccine-doses-administered_final2.csv") 
 
 
 #############################################################
@@ -15,10 +15,6 @@ kv_data <- read_csv("us-daily-covid-vaccine-doses-administered_final2.csv")
 #############################################################
 # define vectors for choice values and labels 
 # can then refer to them in server as well (not just in defining widgets)
-
-# for TAB 1 (line) widgets: 
-# for selectInput, 'choices' object should be a NAMED LIST
-
 
 states <- c("Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida",
             "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana",
@@ -42,36 +38,8 @@ line_choice_values <- c("cumulative_vaccinations", "percentpopcumulative", "dail
 
 line_choice_names <- c("Cumulative Vaccinations", "Cumulative Vaccines Delivered as a Percent of Population", "Vaccinations Administered per Day", 
                        "Vaccinations Administered per Day as a Percent of Population")
+
 names(line_choice_values) <- line_choice_names
-
-#for checkboxGroupInput
-# state_choice_values <- c("alabama",
-#                          "alaska", "arizona", "arkansas", "california", "colorado", "connecticut", "delaware", "florida",
-#                          "georgia", "hawaii", "idaho", "illinois", "indiana", "iowa", "kansas", "kentucky", "louisiana",
-#                          "maine", "maryland", "massachusetts", "michigan", "minnesota", "mississippi", "missouri",
-#                          "montana", "nebraska", "nevada", `new hampshire`, `new jersey`, `new mexico`, `new york`, `north carolina`, `north dakota`, "ohio",
-#                          "oklahoma", "oregon", "pennsylvania", `rhode island`, `south carolina`, `south dakota`, "tennessee", "texas", "utah", "vermont", "virginia", "washington", `west virginia`, "wisconsin", "wyoming")
-# state_choice_names <- c("Alabama",
-#                               "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida",
-#                               "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana",
-#                               "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri",
-#                               "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio",
-#                               "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming")
-
-# state_choice_values <- c("alabama",
-#                          "alaska", "arizona", "arkansas", "california", "colorado", "connecticut", "delaware", "florida",
-#                          "georgia", "hawaii", "idaho", "illinois", "indiana", "iowa", "kansas", "kentucky", "louisiana",
-#                          "maine", "maryland", "massachusetts", "michigan", "minnesota", "mississippi", "missouri",
-#                          "montana", "nebraska", "nevada", "ohio",
-#                          "oklahoma", "oregon", "pennsylvania", "tennessee", "texas", "utah", "vermont", "virginia", "washington",  "wisconsin", "wyoming")
-# state_choice_names <- c("Alabama",
-#                         "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida",
-#                         "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana",
-#                         "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri",
-#                         "Montana", "Nebraska", "Nevada", "Ohio",
-#                         "Oklahoma", "Oregon", "Pennsylvania", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington",  "Wisconsin", "Wyoming")
-# 
-# names(state_choice_values) <- state_choice_names
 
 state_choices <- unique(kv_states_shiny$state)
 
@@ -111,22 +79,17 @@ ui <- navbarPage(
 ############
 server <- function(input,output){
   
-  # TAB 1: LINE
-  # data_for_line <- reactive({
-  #   data <- select(kv_states_shiny, input$linevar)
-  # })
-  # 
+  
   data_for_state <- reactive({
     data <- filter(kv_states_shiny, state %in% input$statevar)
 
   })
-  # 
+  
   output$line <- renderPlot({
     ggplot() +
       geom_line(data = data_for_state(), aes_string(x = "Day", y = input$linevar, color = "state")) +
    
        labs(color = "State",
-         #x = line_choice_names[line_choice_values == input$linevar]
            y = line_choice_names[line_choice_values == input$linevar])
   })
 }
